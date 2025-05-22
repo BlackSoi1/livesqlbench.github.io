2ebdd27f8224cd4aeb69079f95fc316c1d7f7360
BlackSoi1 <lixiaolongjack@gmail.com>
2025-05-23 00:35:23 +0800

    Skip linting and type checking during build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath:
+    process.env.NODE_ENV === "production" ? "/livesqlbench.github.io" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/livesqlbench.github.io" : "",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
@@ -9,4 +24,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
